c2dcecf6e9c1505f22ba60b32136ca553d36ebba
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-02 21:31:00 +0000

    db fixed. owner and vet save and select functions working

--- main/console.py
+++ main/console.py
@@ -8,10 +8,12 @@ import repositories.pet_repository as pet_repository
 import repositories.vet_repository as vet_repository
 
 # functions and stuff
-# owner_1 = Owner('Lewis', 'Halstead', '32 Glass Street, Glasgow, G428UF', '07823513772')
-# owner_repository.save(owner_1)
-# owner_repository.select_all()
+owner_1 = Owner('Lewis', 'Halstead', '32 Glass Street, Glasgow, G428UF', '07823513772')
+owner_repository.save(owner_1)
+owner_repository.select_all()
 
-
+vet_1 = Vet('Jeff', 'Bezos', 'Awful vet. Only here to take peoples money.', 'Snake')
+vet_repository.save(vet_1)
+vet_repository.select_all()
 
 pdb.set_trace()
--- main/db/veterinary_db.sql
+++ main/db/veterinary_db.sql
@@ -1,13 +1,13 @@
+DROP TABLE IF EXISTS pets;
 DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS vets;
-DROP TABLE IF EXISTS pets;
 
 CREATE TABLE vets (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     description VARCHAR(255),
-    favourite_animal VARCHAR(255)
+    fav_animal VARCHAR(255)
 );
 
 CREATE TABLE owners (
@@ -20,10 +20,11 @@ CREATE TABLE owners (
 
 CREATE TABLE pets (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    pet_name VARCHAR(255),
     age INT,
-    type VARCHAR(255),
+    pet_type VARCHAR(255),
     issues VARCHAR(255),
-    notes VARCHAR(255)
-
+    notes VARCHAR(255),
+    vet_id INT REFERENCES vets(id) ON DELETE CASCADE,
+    owner_id INT REFERENCES owners(id) ON DELETE CASCADE
 );

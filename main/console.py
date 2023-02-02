9d44d1899f0ff370ac092a9170a7777116e13667
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-02 22:07:48 +0000

    repo's working. db links working.

--- main/console.py
+++ main/console.py
@@ -10,10 +10,14 @@ import repositories.vet_repository as vet_repository
 # functions and stuff
 owner_1 = Owner('Lewis', 'Halstead', '32 Glass Street, Glasgow, G428UF', '07823513772')
 owner_repository.save(owner_1)
-owner_repository.select_all()
+# owner_repository.select_all()
 
 vet_1 = Vet('Jeff', 'Bezos', 'Awful vet. Only here to take peoples money.', 'Snake')
 vet_repository.save(vet_1)
-vet_repository.select_all()
+# vet_repository.select_all()
+
+pet_1 = Pet(owner_1, vet_1, 'Steve', 6, 'Goat', 'Low Focus', 'Prescribed Cocaine')
+pet_repository.save(pet_1)
+pet_repository.select_all()
 
 pdb.set_trace()
--- main/repositories/pet_repository.py
+++ main/repositories/pet_repository.py
@@ -1,11 +1,13 @@
+import pdb
 from db.run_sql import run_sql
 from models.pet import Pet
 import repositories.owner_repository as owner_repository
 import repositories.vet_repository as vet_repository
 
 def save(pet):
-    sql = 'INSERT INTO pets (owner, vet, name, age, type, issues, notes) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id'
+    sql = 'INSERT INTO pets (owner_id, vet_id, pet_name, age, pet_type, issues, notes) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id'
     values = [pet.owner.id, pet.vet.id, pet.name, pet.age, pet.type, pet.issues, pet.notes]
+    pdb.set_trace()
     results = run_sql(sql, values)
     pet.id = results[0]['id']
     return pet
@@ -16,3 +18,7 @@ def select_all():
     results = run_sql(sql)
     for row in results:
         owner = owner_repository.select(row['owner_id'])
+        vet = vet_repository.select(row['vet.id'])
+        pet = Pet(owner, vet, row['name'], row['age'], row['type'], row['issues'], row['notes'], row['id'])
+        pets.append(pet)
+    return pets

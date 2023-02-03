27917bd0c4672eebc8b516ba3a9155256e2b6798
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-03 12:07:38 +0000

    updated controllers and templates. basic pages working

--- main/controllers/owner_controller.py
+++ main/controllers/owner_controller.py
@@ -4,3 +4,12 @@ import repositories.owner_repository as owner_repository
 
 owners_blueprint = Blueprint('owners', __name__)
 
+@owners_blueprint.route('/owners')
+def owners():
+    owners = owner_repository.select_all()
+    return render_template('owners/index.html', owners=owners)
+
+@owners_blueprint.route('/owners/<id>')
+def view_owner(id):
+    owner = owner_repository.select(id)
+    return render_template('owners/show.html', owner=owner)
--- main/controllers/pet_controller.py
+++ main/controllers/pet_controller.py
@@ -4,3 +4,12 @@ import repositories.pet_repository as pet_repository
 
 pets_blueprint = Blueprint('pets', __name__)
 
+@pets_blueprint.route('/pets')
+def pets():
+    pets = pet_repository.select_all()
+    return render_template('pets/index.html', pets=pets)
+
+@pets_blueprint.route('/pets/<id>')
+def view_pet(id):
+    pet = pet_repository.select(id)
+    return render_template('/pets/show.html', pet=pet)

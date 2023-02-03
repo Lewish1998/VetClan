d8309581fe90d7a99df86d1b12d1df937e7f3fb6
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-03 11:31:24 +0000

    vet template working with vet controller

--- main/controllers/vet_controller.py
+++ main/controllers/vet_controller.py
@@ -4,3 +4,12 @@ import repositories.vet_repository as vet_repository
 
 vets_blueprint = Blueprint('vets', __name__)
 
+@vets_blueprint.route('/vets')
+def vets():
+    vets = vet_repository.select_all()
+    return render_template('vets/index.html', vets=vets)
+
+@vets_blueprint.route('/vets/<id>')
+def view_vet(id):
+    vet = vet_repository.select(id)
+    return render_template('/vets/show.html', vet = vet)
